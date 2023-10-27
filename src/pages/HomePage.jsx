464598c002edb8acc37c6eb17a4f890504e87427
Roberto Lovera <rlovera1cursos@gmail.com>
2023-10-27 01:52:23 -0400

    home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,7 @@ const HomePage = () => {
       <main className="row">
         <article className="col">
           <p>Contenido de la pagina de inicio</p>
+          <img src="src/img/manos1.jpg" />
         </article>
       </main>
     </>
