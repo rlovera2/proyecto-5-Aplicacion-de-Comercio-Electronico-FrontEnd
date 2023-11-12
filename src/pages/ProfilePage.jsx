e9b01e8d4b035f371f4939dc1568cc4d2b2eac55
Roberto Lovera <rlovera1cursos@gmail.com>
2023-11-12 09:15:35 -0500

    login profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -10,9 +10,9 @@ const ProfilePage = () => {
       <Header title="User Profile" />
       <br />
       <main className="row">
-        <div className="card mb-3" style={{ maxWidth: 540 }}>
+        <div className="card mb-3">
           <div className="row g-0">
-            <div className="col-md-4">
+            <div className="col-md-3 logoUser">
               <img
                 src={logoUser}
                 className="img-fluid rounded-start"
